319b5de7816922ce96912a6e0adec9f451fc4dde
minge <heminghu00@126.com>
2024-05-05 17:24:17 +0800

    prettify markdown

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS(), react()],
+  integrations: [UnoCSS()],
+  markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      wrap: true,
+    },
+  },
 });
--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig, presetMini } from 'unocss';
+import { defineConfig, presetIcons, presetMini, presetTypography } from 'unocss';
 
 export default defineConfig({
   presets: [
     presetMini({
       dark: 'class',
     }),
+    presetTypography(),
+    presetIcons(),
   ],
 });
