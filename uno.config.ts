3cdacc70b184fb36dfb186965c76afc44d3fc6d9
minge <heminghu00@126.com>
2024-05-06 22:32:19 +0800

    fix: override css in typography

--- uno.config.ts
+++ uno.config.ts
@@ -5,7 +5,22 @@ export default defineConfig({
     presetMini({
       dark: 'class',
     }),
-    presetTypography(),
+    presetTypography({
+      cssExtend: {
+        ':not(pre) > code::before,:not(pre) > code::after': {
+          content: '&nbsp;&nbsp;',
+        },
+        ':not(pre) > code': {
+          'background-color': '#e5e5e5',
+          'word-wrap': 'break-word',
+          'padding': '.1rem .3rem .2rem',
+          'border-radius': '.2rem',
+        },
+        '.dark :not(pre) > code': {
+          'background-color': '#404040',
+        },
+      },
+    }),
     presetIcons(),
   ],
 });
