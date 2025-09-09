e874f11aa2896d84e00107e8811b2c9c1971b008
mingeme <heminghu00@outlook.com>
2025-09-09 15:02:01 +0800

    feat(analytics): add umami tracking integration

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
+import umami from '@yeskunall/astro-umami';
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [UnoCSS()],
+  integrations: [UnoCSS(), umami({ id: '5b0216af-471b-4774-947c-3fa8879f0243' })],
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
